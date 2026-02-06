f47b22377592cb835820a12e5498a529c763cfc4
Rupanshi Manchanda <rupanshi.manchanda@yahoo.com>
2026-02-06 20:09:55 +0530

    Fix HTML id and selector issues

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const envelope = document.getElementById("envelope-container");
 const letter = document.getElementById("letter-container");
 const noBtn = document.querySelector(".no-btn");
-const yesBtn = document.querySelector('.btn[alt="yes"]');
+const yesBtn = document.querySelector([".yes-btn"]);
 
 const title= document.getElementById("letter-title");
 const catImg = document.getElementById("letter-cat");
